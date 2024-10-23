f6622fec7489900dcf69b834346513af70a2eb9b
Rajdeep1109 <majumderajdeep2@gmail.com>
2024-10-23 20:48:28 +0530

    Profile update
    cleaned profile using button components

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,22 +1,22 @@
 import React from "react";
+import Button from "../context/authContext/button";
 
 function Profile() {
   return(
     <>
     <div class="m-5 ml-[19px] bg-white rounded-lg shadow-lg p-5 w-full">
-      <div class="text-center mb-[19px]">
-        <div class="object-cover bg-slate-400 w-full h-[200px] mb-[20px]">
-          <img src="src\Assets\background.jpg" class="w-full h-full object-cover"/>
-          <div class="relative">
-            <label class="absolute w-[19px] h-[19px] right-0 rounded-md cursor-pointer bg-slate-400 appearance-none">+</label>
+      <div class="flex flex-col items-center justify-center align-middle text-center mb-[19px]">
+        <div class="object-cover  w-full h-[200px] mb-[20px]">
+          <img src="../src/Assets/background.jpg" class="w-full h-full object-cover"/>
+          <div class="absolute flex justify-end">
+            <Button title="+"/>
           </div>
         </div>
-        <img class="cover w-[150px] h-[150px]" src="src\icons\pfp.png"/>
+        <img class="cover w-[150px] h-[150px]" src="../src/icons/pfp.png"/>
           <h1 class="block text-center">Loading....</h1>
           <h1 class="block text-center">@email</h1>
-          <div>
-            <label class="inline-flex items-center appearance-none bg-gradient-to-r from-[#FF512F] via-[#DD2476] to-[#FF512F] mt-1 p-2.5 px-5 text-center uppercase transition duration-500 bg-[200%] text-white font-sans rounded-[19px]">
-              <img class="w-[20px] h-[20px]" src="src\icons\edit.png"/>Edit Profile</label>
+          <div className="flex justify-center">
+            <Button title="Edit profile"/>
           </div>
           <p>Bio about their users</p>
       </div>
@@ -49,12 +49,12 @@ function Profile() {
         </div>
       </div>
       <div class="flex justify-center align-middle">
-        <label class="inline-flex items-center appearance-none bg-gradient-to-r from-[#FF512F] via-[#DD2476] to-[#FF512F] mt-1 p-2.5 px-5 text-center uppercase transition duration-500 bg-[200%] text-white font-sans rounded-[19px]">
-        <img class="w-[20px] h-[20px]" src="src\icons\edit.png"/>Edit Pet details</label>
+        <Button title="Edit Pet profile"/>
       </div>
       <div class="mb-[20px]">
         <h3>Pet Profiles</h3>
-        <div class="bg-slate-400 p-[10px] rounded-lg">
+        <div class="flex justify-between bg-gradient-to-r from-purple-400 to-pink-400 p-[10px] rounded-lg">
+        <div class="flex flex-col gap-2 text-white">
         <p>
           <strong>Age:</strong>"3 years"
         </p>
@@ -64,7 +64,8 @@ function Profile() {
         <p>
           <strong>Medical Checkup:</strong>"Healthy"
         </p>
-        <img class="w-[100px] h-auto" src="src\Assets\catto.jpg"/>
+        </div>
+        <img class="w-[100px] h-auto rounded-md" src="../src/Assets/catto.jpg"/>
         </div>
       </div> 
     </div>
