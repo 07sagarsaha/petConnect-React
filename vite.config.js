80ba094b806e6810c60f005616b77204eade07fd
Sagar Saha <ss2749968@gmail.com>
2025-01-18 12:06:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "firebase-messaging-sw.js",
-      injectManifest: {
-        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-      },
-      manifest: {
-        name: "Pet Connect",
-        short_name: "PetConnect",
-        description: "A platform to connect pet lovers",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
