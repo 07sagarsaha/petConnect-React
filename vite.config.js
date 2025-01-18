bed67d3da5970941e9e1bd3b314a3813b06dc155
Sagar Saha <ss2749968@gmail.com>
2025-01-18 12:34:27 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "firebase-messaging-sw.js",
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ],
   assetsInclude: ["**/*.glb"],
 });
