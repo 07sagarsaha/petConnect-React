9b4a8d949824fae4f7b7ab1792d0a98fd9ccc735
Subham Chakraborty <155424624+TheCoderSubham@users.noreply.github.com>
2025-05-18 18:21:46 +0530

    Attempted at making a PWA app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PetConnect',
+        short_name: 'PetConnect',
+        description: 'PetConnect is a social media platform for pet owners to connect with other pet owners.',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/Assets/logo (1).jpg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/Assets/logo (1).jpg',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/Assets/logo (1).jpg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+      }
+    })],
   assetsInclude: ["**/*.glb"],
 });
